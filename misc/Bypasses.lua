719df0598cf027ee2971b53949f6ce3ea28645e5
f4jka <117294974+f4jka@users.noreply.github.com>
2022-11-02 18:38:09 +0100

    Update Bypasses.lua

--- misc/Bypasses.lua
+++ misc/Bypasses.lua
@@ -9,8 +9,10 @@ return {
     ["ass"] = "as{{AIeIXzVZx:s}}",
     ["beaner"] = "beane{{AIeIXzVZx:r}}", -- Unstable
     ["bitch"] = "bit{{AIeIXzVZx:ch}}",
+    ["bitches"] = "bit{{AIeIXzVZx:ches}}",
     ["blowjob"] = "blow{{AIeIXzVZx:job}}",
     ["boob"] = "boo{{AIeIXzVZx:b}}",
+    ["boobs"] = "boo{{AIeIXzVZx:bs}}",
     ["boner"] = "bon{{AIeIXzVZx:er}}", -- Unstable
     ["cock"] = "co{{AIeIXzVZx:ck}}",
     ["cunt"] = "cu{{AIeIXzVZx:nt}}",
@@ -33,13 +35,16 @@ return {
     ["motherfucker"] = "motherf{{zczxczvzaiei:ucker}}",
     ["nazi"] = "na{{AIeIXzVZx:zi}}",
     ["nigga"] = "ni{{AIeIXzVZx:gga}}",
+    ["niggas"] = "ni{{AIeIXzVZx:ggas}}",
     ["nigger"] = "ni{{AIeIXzVZx:gger}}",
+    ["niggers"] = "ni{{AIeIXzVZx:ggers}}",
     ["piss"] = "pi{{zczxczvz:ss}}",
     ["penis"] = "pe{{AIeIXzVZx:nis}}",
     ["pussy"] = "pu{{AIeIXzVZx:ssy}}",
     ["rap"] = "ra{{AIeIXzVZx:p}}",  -- Unstable
     ["retard"] = "reta{{AIeIXzVZx:rd}}",
     ["sex"] = "se{{AIeIXzVZx:x}}",
+    ["sexy"] = "se{{AIeIXzVZx:xy}}",
     ["shit"] = "sh{{AIeIXzVZx:it}}",
     ["slut"] = "sl{{zczxczvz:ut}}",
     ["sperm"] = "spe{{AIeIXzVZx:rm}}",
